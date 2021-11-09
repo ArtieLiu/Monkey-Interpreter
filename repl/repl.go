c54ad54dd1cf517804948678274d0613014d5453
Yinan Liu <yinan97@outlook.com>
2021-11-09 21:25:44 +0800

    1109 update repl to show parsed results instead of lexed results

--- repl/repl.go
+++ repl/repl.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"io"
 	"monkeyinterpreter/lexer"
-	"monkeyinterpreter/token"
+	"monkeyinterpreter/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -20,8 +21,22 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		if len(p.Errors())!=0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _,msg := range errors	{
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
