6ed59cff8301f4fc39de45e836a0a18f8cd5f8ad
Yinan Liu <yinan97@outlook.com>
2021-11-13 16:50:55 +0800

    1113 print evaluated value instead of program literal

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkeyinterpreter/evaluator"
 	"monkeyinterpreter/lexer"
 	"monkeyinterpreter/parser"
 )
@@ -25,18 +26,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		program := p.ParseProgram()
 
-		if len(p.Errors())!=0 {
+		if len(p.Errors()) != 0 {
 			printParseErrors(out, p.Errors())
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
 func printParseErrors(out io.Writer, errors []string) {
-	for _,msg := range errors	{
-		io.WriteString(out, "\t" + msg + "\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
