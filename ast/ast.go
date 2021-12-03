3703231cfaecd0568331b5d57d030916bde93055
Yinan Liu <yinan97@outlook.com>
2021-12-03 21:42:20 +0800

    1203 parse string literal

--- ast/ast.go
+++ ast/ast.go
@@ -30,6 +30,21 @@ func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) TokenLiteral() string {
+	return sl.Token.Literal
+}
+
+func (sl *StringLiteral) String() string {
+	return sl.Token.Literal
+}
+
+func (sl *StringLiteral) expressionNode() {}
+
 type Program struct {
 	Statements []Statement
 }
